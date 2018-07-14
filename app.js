15e6eef971f65672fd31ed037dd449c084f06c1f
Robyn <oddert@gmail.com>
2018-07-14 13:44:41 +0100

    Added PinApathy pinterest clone added to folio page

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express     = require('express'),
     app         = express(),
     bodyParser  = require('body-parser'),
-    ejs         = require('ejs');
+    ejs         = require('ejs'),
+    path        = require('path');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/folios'));
+app.use(express.static(path.join(__dirname + '/img')));
+// app.use(express.static(__dirname + '/pdfs'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  // res.render('index');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(3000, () => {
