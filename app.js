f4ad520746b4d6991121a062d102484f7c6e57de
Robyn <oddert@gmail.com>
2018-11-05 12:09:58 +0000

    fixed server functionality, still invalid due to serving root directory but this requires alot more work

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-var express     = require('express'),
-    app         = express(),
-    bodyParser  = require('body-parser'),
-    ejs         = require('ejs'),
-    path        = require('path');
+var express     = require('express')
+  , app         = express()
+  , bodyParser  = require('body-parser')
+  , path        = require('path')
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/folios'));
-app.use(express.static(path.join(__dirname + '/img')));
-// app.use(express.static(__dirname + '/pdfs'));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '/')))
 
 app.get('/', (req, res) => {
-  // res.render('index');
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
 
-app.listen(3000, () => {
-  console.log('Server initialised on port 3000...');
-});
+const PORT = process.env.PORT || 3000
+app.listen(
+  PORT
+, () => console.log(`${new Date().toLocaleTimeString('en-GB')}: Server initialised on port ${PORT}...`)
+)
